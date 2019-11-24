532aeb94166a722e3b0591d1a46ad4b1c20604aa
zyv42 <zyv42@sdf.org>
2019-11-24 19:18:59 +0200

    add custom queries for notification service

--- notification-service/src/main/java/com/eugenarium/notification/persistence/repository/RecipientRepository.java
+++ notification-service/src/main/java/com/eugenarium/notification/persistence/repository/RecipientRepository.java
@@ -12,9 +12,11 @@ public interface RecipientRepository extends CrudRepository<Recipient, String> {
 
     Recipient findByAccountName(String accountName);
 
-    @Query("")
+    @Query("SELECT gog.recipients FROM gog.recipients WHERE scheduledNotifications.BACKUP.lastNotified < (CURRENT_DATE -" +
+            "INTERVAL 'scheduledNotifications.BACKUP.frequency days')")
     List<Recipient> findReadyForBackup();
 
-    @Query("")
+    @Query("SELECT gog.recipients FROM gog.recipients WHERE scheduledNotifications.REMIND.lastNotified < (CURRENT_DATE -" +
+            "INTERVAL 'scheduledNotifications.REMIND.frequency days')")
     List<Recipient> findReadyForRemind();
 }
